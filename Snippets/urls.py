3c8f94c1dc7311460f4c3fec6a1e80eb8625daf6
Sedelnikova.MS <marisedas@inbox.ru>
2022-05-27 20:58:19 +0300

    requirements

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
 
 from MainApp import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='adding'),
     path('snippets/list', views.snippets_page, name='list'),
